176befe7eec97f8a2ac4fd426bd47bebc34294f9
Class-util <wb1945621909@aliyun.com>
2021-01-02 16:46:54 +0800

    2021.1.2: 单链表练习实现代码

--- Day04/Test01.java
+++ Day04/Test01.java
@@ -1,5 +1,7 @@
 package Day04;
 
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -8,5 +10,14 @@ package Day04;
  * Time:18:41
  */
 public class Test01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入A");
+        int a = sc.nextInt();
+        System.out.println("输入B");
+        int b = sc.nextInt();
 
+
+        System.out.println("a+b=" + (a+b));
+    }
 }
