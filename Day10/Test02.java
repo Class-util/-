3aed6cb72adf6b179ff9e4173f365052f1ce1563
Class-util <wb1945621909@aliyun.com>
2021-02-02 18:21:50 +0800

    2021.2.2 ： 归并排序

--- Day10/Test02.java
+++ Day10/Test02.java
@@ -3,6 +3,7 @@ package Day10;
 import Day09.HeapDemo;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,8 +14,15 @@ import java.util.Arrays;
  */
 public class Test02 {
 
+    public static void main(String[] args) {
+        int[] arr = {6,5,4,3,2,1};
+        //quickSort1(arr);
+        mergeSort1(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
     public String name="abc";
-    public static void main(String[] args){
+    public static void main2(String[] args){
         Test02 test=new Test02();
         Test02 testB=new Test02();
         System.out.println(test.equals(testB)+","+test.name.equals(testB.name));
@@ -38,6 +46,115 @@ public class Test02 {
         System.out.println(j.equals(i));
 
     }
+
+    //归并排序非递归
+    public static void merge(int[]arr,int gap){
+        int s1 = 0;
+        int e1 = s1 + gap - 1;
+        int s2 = e1 + 1;
+        int e2 = s2 + gap - 1 < arr.length ? s2 + gap -1 : arr.length-1;
+        int[] tmp = new int[arr.length];
+        int k = 0;
+        while (s2 < arr.length){
+            while (s1 <= e1 && s2 <= e2){
+                if(arr[s1] <= arr[s2]){
+                    tmp[k++] = arr[s1++];
+                }else {
+                    tmp[k++] = arr[s2++];
+                }
+            }
+            while (s1 <= e1){
+                tmp[k++] = arr[s1++];
+            }
+            while (s2 <= e2){
+                tmp[k++] = arr[s2++];
+            }
+            s1 = e2 + 1;
+            e1 = s1 + gap - 1;
+            s2 = e1 + 1;
+            e2 = s2 + gap - 1 < arr.length ? s2 + gap -1 : arr.length-1;
+        }
+        while (s1 < arr.length){
+            tmp[k++] = arr[s1++];
+        }
+        for (int i = 0; i < tmp.length; i++) {
+            arr[i] = tmp[i];
+        }
+    }
+    public static void mergeSort1(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            merge(arr,i);
+        }
+    }
+
+    ///归并排序
+    public static  void mergeSortInternal(int[] arr,int low,int high){
+        if(low >= high)
+            return ;
+        int mid = (low + high) / 2;
+        mergeSortInternal(arr,low,mid);
+        mergeSortInternal(arr,mid+1,high);
+        //合并
+        merge(arr,low,mid,high);
+    }
+
+    public static void merge(int[] arr ,int low ,int mid,int high){
+        int s1 = low;
+        int s2 = mid + 1;
+        int[] tmp = new int[high - low + 1];
+        //数组下标
+        int k = 0;
+        while (s1 <= mid && s2 <= high){
+            if(arr[s1] <= arr[s2]){
+                tmp[k++] = arr[s1++];
+            }else {
+                tmp[k++] = arr[s2++];
+            }
+        }
+        while (s1 <= mid){
+            tmp[k++] = arr[s1++];
+        }
+        while (s2 <= high){
+            tmp[k++] = arr[s2++];
+        }
+        System.arraycopy(tmp, 0, arr, low, tmp.length);
+    }
+
+    public static  void mergeSort(int[] arr){
+        mergeSortInternal(arr,0,arr.length -1);
+    }
+
+
+
+    //快速排序非递归
+    public static void quickSort1(int[] arr){
+        Stack<Integer> stack = new Stack<>();
+        int low = 0;
+        int high = arr.length - 1;
+        int piv = pivot(arr,low,high);
+        if(piv > low + 1){
+            stack.push(low);
+            stack.push(piv -1);
+        }
+        if(piv < high - 1){
+            stack.push(piv + 1);
+            stack.push(high);
+        }
+        while (!stack.isEmpty()){
+            high = stack.pop();
+            low = stack.pop();
+            piv = pivot(arr,low,high);
+            if(piv > low + 1){
+                stack.push(low);
+                stack.push(piv -1);
+            }
+            if(piv < high - 1){
+                stack.push(piv + 1);
+                stack.push(high);
+            }
+        }
+    }
+
 
     //快速排序
     public static void quickSort(int[] arr){
